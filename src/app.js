f58fad3a7efc69b55fe7395141f7efc4b87dbfd4
Willy Samuel Paz Colque <samuelpaz243@gmail.com>
2022-06-16 18:41:57 -0400

    nodejs project

---
+++ src/app.js
@@ -0,0 +1,58 @@
+const { join } = require('path');
+const { v4: uuid } = require('uuid');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', join(__dirname, 'views'));
+app.engine(
+  '.hbs',
+  exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars'),
+  })
+);
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'SUPER_SECRET',
+    genid: function (req) {
+      return uuid(); // use UUIDs for session IDs
+    },
+  })
+);
+app.use(flash());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Global variables
+app.use((req, res, next) => {
+  app.locals.success = req.flash('success');
+  app.locals.message = req.flash('message');
+  next();
+});
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+// public
+app.use(express.static(join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
+});
